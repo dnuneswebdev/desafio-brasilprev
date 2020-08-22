9e7caf42ad579c548793eeca1498f1522dfb5fda
Daniel <dnuneswebdev@gmail.com>
2020-08-22 13:47:59 -0300

    Created pokemon service; added getAllCards method; configured api main url to the envirioment file; added the service to the app module providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './pages/home/home.module';
 import { PokemonModule } from './pages/pokemon/pokemon.module';
+import { PokemonService } from './services/pokemon.service';
 
 @NgModule({
   declarations: [
@@ -12,11 +14,12 @@ import { PokemonModule } from './pages/pokemon/pokemon.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     HomeModule,
     PokemonModule,
   ],
-  providers: [],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
