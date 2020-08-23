79ca5d59bbcbbdbc9ad0c28051e54db235d4d6fa
Daniel <dnuneswebdev@gmail.com>
2020-08-23 12:30:49 -0300

    Created and configured spinner component in shared folder; Applied the component in the pokemon pages; Minor css adjustments

--- src/app/pages/pokemon/pokemon.module.ts
+++ src/app/pages/pokemon/pokemon.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PokemonComponent } from './main/pokemon.component';
 import { PokemonDetailsComponent } from './details/pokemon-details.component';
 import { PokemonRouting } from './pokemon.routing';
+import { SpinnerModule } from 'src/app/shared/components/spinner/spinner.module';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { PokemonRouting } from './pokemon.routing';
   imports: [
     CommonModule,
     PokemonRouting,
+    SpinnerModule
   ]
 })
 export class PokemonModule { }
